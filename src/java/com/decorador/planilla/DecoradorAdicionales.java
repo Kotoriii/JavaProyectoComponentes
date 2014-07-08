168c7b8a7cd7a970203795a2966912b8553c1d22
Andrea Tupini <tupini07@gmail.com>
2014-07-08 15:48:41 -0600

    eliminado proyecto netbeans

--- src/java/com/decorador/planilla/DecoradorAdicionales.java
+++
@@ -1,9 +0,0 @@
-package com.decorador.planilla;
-
-/**
- * @author Deiver Kiel
- */
-public abstract class DecoradorAdicionales extends Salario{
-    @Override
-    public abstract String getDescription();
-}
--- src/java/com/decorador/planilla/DecoradorDeducciones.java
+++
@@ -1,9 +0,0 @@
-package com.decorador.planilla;
-
-/**
- * @author Deiver Kiel
- */
-public abstract class DecoradorDeducciones extends Salario{
-    @Override
-    public abstract String getDescription();
-}
